58809dbf6962dc5ddf0e43867ad67f53e471501d
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2021-06-05 23:06:42 +0100

    Initial commit

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <form action="login.php" method="post">
+    Username: <input type="text" name="username"> <br>
+    Password: <input type="password" name="password">
+    <input type="submit">
+    </form>
+
+    <?php
+
+
+    ?>
+</body>
+</html>
